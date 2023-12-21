b4fe1e57b068d5815108f003ec7bd94d737f5b79
silverhyo <144036029+silverhyo@users.noreply.github.com>
2023-12-21 15:53:40 +0900

    V2

--- script.js
+++ script.js
@@ -144,13 +144,96 @@ classfy.addEventListener('submit', function(event) {
         return true;
     })
     showProduct(productFilter);
+    openPopupWindowActive();
 })
 // ============================================================POPUP WINDOW
 
 makePopupWindow();
+function makePopupWindow(){
+    let previewContainer = document.getElementById('previewContainer');
+    previewContainer.innerHTML ='';
+    productFilter.forEach(item => {
 
-function makePopupWindow() {
-    let previewContainer = $('previewContaienr');
-    
+            let newData00 = document.createElement('div');
+            newData00.classList.add('previewbox');
+            let newData01 = newData00;
+            newData01.dataset.target = item.dataname;
+
+            let newData03 = document.createElement('div');
+            newData03.classList.add('preview');
+            newData01.appendChild(newData03);
+
+            let newImage = new Image ();
+            newImage.src = item.image;
+            newData03.appendChild(newImage);
+            
+            let newData04 = document.createElement('div');
+            newData04.classList.add('closebutton');
+            newData04.innerHTML = "close";
+            newData03.appendChild(newData04);
+
+            let newData05 = document.createElement('br');
+            newData01.appendChild(newData05);
+
+            let newData06 = document.createElement('h2');
+            newData06.innerHTML = item.name;
+            newData01.appendChild(newData06);
+
+            let newData07 = document.createElement('p');
+            newData07.classList.add('flowersummary');
+            newData07.innerHTML = item.nature.color + '의 예쁜 ' + item.price + '원대 ' + item.nature.type + ' 이예요!';
+            newData01.appendChild(newData07);
+
+            let newData08 = document.createElement('br');
+            newData01.appendChild(newData08);
+
+            let newData09 = document.createElement('p');
+            newData09.classList.add('description');
+            newData09.innerHTML = item.nature.description01;
+            newData01.appendChild(newData09);
+
+            previewContainer.appendChild(newData01);
+        
+    })
+ }
+
+// =====================================================OPENPOPUPWINDOWACTIV
+
+openPopupWindowActive();
+function openPopupWindowActive() {
+    let previewContainer = document.getElementById('previewContainer');
+    let flowerList = document.querySelectorAll('#list .item');
+    let previewBox = document.querySelectorAll('.previewbox');
+
+    flowerList.forEach(item => {
+        item.onclick = () => {
+            previewContainer.style.display = "flex";
+            let name = item.getAttribute('data-name');
+            previewBox.forEach(previewBox => {
+                let target = previewBox.getAttribute('data-target');
+                if(name == target) {
+                    previewBox.classList.add('active');
+                };
+            });
+        };
+    });
 
 }
+
+// =======================================================CLOSE POPUPWINDOW
+
+// closePopupWindow();
+// function closePopupWindow() {
+//     let previewContainer = document.getElementById('previewContainer');
+
+let closeButton = document.querySelectorAll('.closebutton');
+
+closeButton.forEach(item => {
+    item.onclick =() => {
+        previewContainer.style.display = "none";
+        let previewBoxActive = document.querySelector('.previewbox.active');
+        previewBoxActive.classList.remove('active');
+    }
+})
+
+
